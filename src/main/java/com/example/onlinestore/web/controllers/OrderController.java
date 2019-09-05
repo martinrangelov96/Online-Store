db93b8b52703012dd1dd8a5b1dfcf18745a46aa0
martinrangelov96 <martin.rangelov96@gmail.com>
2019-09-05 14:45:25 +0300

    Added OrderConstants.java

--- src/main/java/com/example/onlinestore/web/controllers/OrderController.java
+++ src/main/java/com/example/onlinestore/web/controllers/OrderController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.example.onlinestore.constants.Constants.*;
-import static com.example.onlinestore.constants.OrdersConstants.*;
+import static com.example.onlinestore.constants.OrderConstants.*;
 
 @Controller
 @RequestMapping(REQUEST_MAPPING_ORDERS_CONST)
